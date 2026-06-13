99be63e62a0f782ab9f9327452377ba7131d1e24
madhanraj-source <maddy.akm@gmail.com>
2026-06-14 00:02:26 +0530

    Updating vpc module

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -16,6 +16,7 @@ resource "aws_subnet" "public_a" {
 
   tags = {
     Name = "${var.project_name}-public-a"
+    "kubernetes.io/role/elb" = "1"
   }
 }
 
@@ -27,6 +28,7 @@ resource "aws_subnet" "public_b" {
 
   tags = {
     Name = "${var.project_name}-public-b"
+    "kubernetes.io/role/elb" = "1"
   }
 }
 
@@ -37,6 +39,7 @@ resource "aws_subnet" "private_a" {
 
   tags = {
     Name = "${var.project_name}-private-a"
+    "kubernetes.io/role/internal-elb" = "1"
   }
 }
 
@@ -47,5 +50,82 @@ resource "aws_subnet" "private_b" {
 
   tags = {
     Name = "${var.project_name}-private-b"
+    "kubernetes.io/role/internal-elb" = "1"
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_a.id
+
+  tags = {
+    Name = "${var.project_name}-nat"
+  }
+
+  depends_on = [
+    aws_internet_gateway.this
+  ]
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private.id
+}
+

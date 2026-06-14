b8117c4d99a4bf6e8c1065395df29ce53b7e77cb
madhanraj-source <maddy.akm@gmail.com>
2026-06-14 11:57:48 +0530

    Adding eks module

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -22,3 +22,12 @@ module "ecr" {
   project_name = var.project_name
   environment  = var.environment
 }
+
+module "eks" {
+  source = "../../modules/eks"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  private_subnet_ids = module.vpc.private_subnet_ids
+}

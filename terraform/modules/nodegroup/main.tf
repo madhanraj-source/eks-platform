1fe0eb245a0b67d73bb63651e85cbc81c836ab45
madhanraj-source <maddy.akm@gmail.com>
2026-06-14 15:32:58 +0530

    Changed instance type

--- terraform/modules/nodegroup/main.tf
+++ terraform/modules/nodegroup/main.tf
@@ -41,12 +41,12 @@ resource "aws_eks_node_group" "this" {
 
   subnet_ids = var.private_subnet_ids
 
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.micro"]
 
   scaling_config {
     desired_size = 2
     min_size     = 2
-    max_size     = 4
+    max_size     = 2
   }
 
   capacity_type = "ON_DEMAND"

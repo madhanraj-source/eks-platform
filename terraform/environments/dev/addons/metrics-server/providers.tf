770f5750b1ecae10d28d99ab5b3828c9096832e7
madhanraj-source <maddy.akm@gmail.com>
2026-06-22 10:35:03 +0530

    Updated metrics-server provider and state file

--- terraform/environments/dev/addons/metrics-server/providers.tf
+++ terraform/environments/dev/addons/metrics-server/providers.tf
@@ -1,28 +1,3 @@
-terraform {
-  required_version = ">= 1.6"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.30"
-    }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-2"
-}
-
 data "aws_eks_cluster" "this" {
   name = data.terraform_remote_state.infra.outputs.cluster_name
 }
@@ -32,15 +7,23 @@ data "aws_eks_cluster_auth" "this" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  host = data.aws_eks_cluster.this.endpoint
+
+  cluster_ca_certificate = base64decode(
+    data.aws_eks_cluster.this.certificate_authority[0].data
+  )
+
+  token = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
+    host = data.aws_eks_cluster.this.endpoint
+
+    cluster_ca_certificate = base64decode(
+      data.aws_eks_cluster.this.certificate_authority[0].data
+    )
+
+    token = data.aws_eks_cluster_auth.this.token
   }
 }
--- terraform/environments/dev/addons/metrics-server/remote-state.tf
+++ terraform/environments/dev/addons/metrics-server/remote-state.tf
@@ -2,8 +2,8 @@ data "terraform_remote_state" "infra" {
   backend = "s3"
 
   config = {
-    bucket = "eks-demo-terraform-state"
-    key    = "dev/infra/terraform.tfstate"
+    bucket = "eks-platform-tfstate-maddy-2026"
+    key    = "dev/terraform.tfstate"
     region = "us-east-2"
   }
 }

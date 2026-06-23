129ecad63390e374694265d10869f631736bda75
madhanraj-source <maddy.akm@gmail.com>
2026-06-23 19:29:45 +0530

    Updated addon providers

--- terraform/environments/dev/addons/metrics-server/providers.tf
+++ terraform/environments/dev/addons/metrics-server/providers.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_version = ">= 1.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.14"
+    }
+  }
+}
+
 data "aws_eks_cluster" "this" {
   name = data.terraform_remote_state.infra.outputs.cluster_name
 }

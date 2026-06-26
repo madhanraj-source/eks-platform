802306824322916f5b01b2490c4591bc8737e3a5
madhanraj-source <maddy.akm@gmail.com>
2026-06-26 16:38:57 +0530

    Updated karpenter addon

--- terraform/environments/dev/addons/karpenter/main.tf
+++ terraform/environments/dev/addons/karpenter/main.tf
@@ -1,4 +1,31 @@
+data "aws_iam_policy_document" "karpenter_assume_role" {
+  statement {
+    effect = "Allow"
 
-locals {
-  addon_name = "karpenter"
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    principals {
+      type = "Federated"
+      identifiers = [
+        data.terraform_remote_state.infra.outputs.oidc_provider_arn
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(data.terraform_remote_state.infra.outputs.oidc_provider, "https://", "")}:sub"
+
+      values = [
+        "system:serviceaccount:kube-system:karpenter"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "karpenter_controller" {
+  name = "${data.terraform_remote_state.infra.outputs.cluster_name}-karpenter-controller-role"
+
+  assume_role_policy = data.aws_iam_policy_document.karpenter_assume_role.json
 }
